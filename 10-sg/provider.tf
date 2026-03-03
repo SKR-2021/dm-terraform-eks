567279ca4993c297a2a5cd39dc214f7d46960247
karunakar2000 <karunakar2000@gmail.com>
2026-03-03 14:25:32 +0200

    10-sg

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
-    key    = "roboshop-eks-sg"
+    bucket = "dotmart-remote-dev"
+    key    = "dotmart-eks-sg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
